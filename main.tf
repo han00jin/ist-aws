697b156c80053a0af134f319b88c2499904ee8a9
han00jin <94149750+han00jin@users.noreply.github.com>
2022-03-14 17:29:15 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,19 +8,31 @@ provider "aws" {
 resource "aws_instance" "InnovationDay01" {
   ami           = "ami-01b20f5ea962e3fe7"
   instance_type = "t2.micro"
+     tags = {
+        Name = "InnovationDay01"
+    }
 }
 
 resource "aws_instance" "InnovationDay02" {
   ami           = "ami-01b20f5ea962e3fe7"
   instance_type = "t2.micro"
+     tags = {
+        Name = "InnovationDay02"
+    }
 }
 
 resource "aws_instance" "InnovationDay03" {
   ami           = "ami-01b20f5ea962e3fe7"
   instance_type = "t2.micro"
+     tags = {
+        Name = "InnovationDay03"
+    }
 }
 
 resource "aws_instance" "InnovationDay04" {
   ami           = "ami-01b20f5ea962e3fe7"
   instance_type = "t2.micro"
+     tags = {
+        Name = "InnovationDay04"
+    }
 }
